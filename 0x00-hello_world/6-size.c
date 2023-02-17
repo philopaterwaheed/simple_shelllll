b56008884b0cdea8ccd2d89b5eb6d9cf511f36b3
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-16 17:07:37 -0800

    bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,12 @@
-#include <stdio.h> 
- int main () {} 
+#include <stdio.h>
+/*
+ *main - simple function prints the size of various types on the computer it is compiled and run on
+ *Return: 0 exit code 
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)",sizeof(char));
+	printf("Size of a int: %d byte(s)",sizeof(int));
+	printf("Size of a long long: %d byte(s)",sizeof(long long));
+	printf("Size of a float: %d byte(s)",sizeof(float));
+}
