5a21814bed794aaa428db3d2f990d39e18836f91
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-16 16:50:08 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
-#include <stdio.h> 
- int main () {} 
+#include <stdio.h>
+/*
+ * main function simple that prints msg to conslor 
+ * Return: 0 exit code
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+} 
