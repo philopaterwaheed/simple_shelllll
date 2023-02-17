b8eb6d5a9530c0f98b3daeefb0a26d2834192d60
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-16 16:37:08 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h> 
-/*
+
+/**
  *main - simple function to print msg to stout
- *returns 0 exit code
+ *Return: 0 exit code
  */
 int main (void)
 {
-	puts ("\"Programming is like building a multilingual puzzle\n");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return 0 ; 
 } 
