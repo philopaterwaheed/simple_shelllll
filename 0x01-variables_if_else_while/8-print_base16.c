2b25b80297ebd4f4c60bf30c46def6f24acde886
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-17 16:42:15 -0800

    16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
-int main(void) 
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Return: 0
+ *
+ */
+int main(void)
 {
+	int c;
+
+	for (c = 48; c < 58; c++)
+		putchar(c);
+	for (c = 97; c < 103; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
 }
