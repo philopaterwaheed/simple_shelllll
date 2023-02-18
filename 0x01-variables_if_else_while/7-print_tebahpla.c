3ff9a6aeb0186724fd5d56659fac3ab79870d428
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-17 16:38:10 -0800

    rev

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
-int main(void) 
+/**
+ * main - prints the alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
 {
+	int c = 122;
+
+	for (c = 122; c >= 97; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
