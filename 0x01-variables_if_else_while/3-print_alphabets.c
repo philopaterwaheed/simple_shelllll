da5ae3ccbca202d41cbcbdf9378756bc59df716a
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-17 16:08:24 -0800

    upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase
+ * Return: 0
+ */
 int main(void) 
 {
+	int c = 97;
+
+	for (c = 97; c < 123; c++)
+		putchar(c);
+	for (c = 65; c < 91; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
 }
