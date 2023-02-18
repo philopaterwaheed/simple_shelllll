788349daa80ac6fdad6a0ccb3dbfc75396b05c3c
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-17 16:17:09 -0800

    e-q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
-int main(void) 
+/**
+ * main - prints the alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
 {
+	int c = 97;
+
+	for (c = 97; c < 123; c++)
+	{
+		if ( c >= 101 && c <= 113)
+			continue;
+		putchar(c);
+		putchar('\n');
+	}
+	return (0);
 }
