30cae8c04ae8d5b13e89ee33ee9a87f97b341529
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-17 16:29:32 -0800

    0-10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,11 +3,12 @@
  * main -prints all single digit numbers of base 10 starting from 0
  * Return: 0
  */
-int main(void) 
+int main(void)
 {
 	int i;
 
-	for (i =0; i<= 10; i++)
+	for (i = 0; i <= 10; i++)
 		printf("%d", i);
+	putchar('\n');
 	return (0);
 }
