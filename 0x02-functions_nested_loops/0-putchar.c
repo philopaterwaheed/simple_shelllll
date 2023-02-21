94fd6311b174f052ac2f9bac0501a72c92348056
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-21 15:02:41 -0800

    _pusts

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* main - does some thing 
+* main - does some thing
 * Return: 0
 */
 int main(void)
 {
 	int i;
-	char *msg = "_putcahr";
+	char *msg = "_putchar";
 
 	for (i = 0; i < 8; i++)
 		_putchar(msg[i]);
