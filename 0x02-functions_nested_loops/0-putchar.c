0177e41f239451423b4f7f4e7258748a3c47b726
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-21 14:55:07 -0800

    _pusts

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 /**
 * main - does some thing 
 * Return: 0
 */
 int main(void)
 {
+	int i;
+	char msg[64] = "_putcahr";
+
+	for (i = 0; i < 8; i++)
+		_putschar(msg[i]);
+
+	_putschar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,1 @@
+int _putschar(char c);
