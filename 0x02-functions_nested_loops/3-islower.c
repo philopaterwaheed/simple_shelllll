79332a38f5384a7f4553708dd69c0c3dff9e8447
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-21 15:56:25 -0800

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* islower - does some thing
+* _islower - does some thing
 * Return: 1 on sucess
 * Return: 0 on fail
 */
-int main (int c)
+int _islower(int c)
 {
 	return (c >= 97 && c<=122 ? 1 : 0);
 }
