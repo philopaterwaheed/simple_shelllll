2fd21267c2ebe70264353b80734d61d9dae0ee3e
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-21 15:44:11 -0800

    10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,14 @@
 * main - does some thing 
 * Return: void
 */
-int main(void)
+void print_alphabet_x10(void)
 {
 	int i;
 	int c;
 
 	for (i =0; i < 10 ; i++)
 	{
-		for (c = 'a'; c <= 'z'; z++)
+		for (c = 'a'; c <= 'z'; c++)
 			_putchar(c);
 		_putchar('\n');
 	}
