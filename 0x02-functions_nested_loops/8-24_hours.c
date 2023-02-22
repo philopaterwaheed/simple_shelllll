e363b0193860dc7ceddf0ff7989e0c1f05f2f055
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-21 16:52:28 -0800

    hour

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,38 @@
-#include <stdio.h>
+#include "main.h"
 /**
-* main - does some thing 
-* Return: 0
-*/
-int main(void)
+ * print_int - print 2 digits number as ascii string
+ * @n: the number to print
+ * Return: void
+ */
+void print_int(int n)
 {
-	return (0);
+	int a, b;
+
+	b = n % 10;
+	a = n / 10;
+
+	_putchar(a + 48);
+	_putchar(b + 48);
+}
+
+/**
+ * jack_bauer - print every minute of the day
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i;
+
+	for (i = 0; i < 1440; i++)
+	{
+		int min, h;
+
+		h = i / 60;
+		min = i % 60;
+
+		print_int(h);
+		_putchar(':');
+		print_int(min);
+		_putchar('\n');
+	}
 }
