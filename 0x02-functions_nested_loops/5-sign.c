c329e4604d2d24517d35a4cbeec09bf4a8ee0da3
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-21 16:26:47 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,12 +5,21 @@
 * Return: 1 if @n is a lowercase
 * else return 0
 */
-int print_sign(int i)
+int print_sign(int n)
 {
-	if (i > 0)
+	if (n > 0)
+	{
+		_putchar('+');
 		return (1);
-	else if (i < 0)
+	}
+	else if (n < 0)
+	{
+		_putchar('+');
 		return (-1);
+	}
 	else
+	{
+		_putchar('+');
 		return (0);
+	}
 }
