5254cc77b60dcb4f60e555271f8ebf28edc83a54
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-21 16:21:12 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 /**
-* main - does some thing 
-* Return: 0
+* print_sign - does some thing
+* @n: used to check if lower
+* Return: 1 if @n is a lowercase
+* else return 0
 */
-int main(void)
+int print_sign(int i)
 {
-	return (0);
+	if (i > 0)
+		return (1);
+	else if (i < 0)
+		return (-1);
+	else
+		return (0);
 }
