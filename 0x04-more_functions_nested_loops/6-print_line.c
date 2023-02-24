c4cfd4693185bb620fe9aac028992abdeebcba9d
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-23 17:39:16 -0800

    snack

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,12 +6,13 @@
 */
 void print_line(int n)
 {
-	char i;
+	int i;
 
-	for (i = 0; i <= n; i++)
+	if (n < 0)
+		n = 0;
+	for (i = 0; i < n; i++)
 	{
 		_putchar('_');
 	}
-	_putchar('$');
 	_putchar('\n');
 }
