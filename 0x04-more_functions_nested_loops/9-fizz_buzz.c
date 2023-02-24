6df881f8ceef43e41977274c49caa0e1a9f8f41b
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-23 18:03:05 -0800

    s

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+
 /**
-* main - does some thing
-* Return: 0
-*/
+ * main - does something
+ * Return: void
+ */
 int main(void)
 {
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		printf("%c", i < 100 ? ' ' : '\n');
+	}
+
 	return (0);
 }
