e3dd58dc8ce34f937ae1a5a097d862cd9d9aa927
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-24 16:18:46 -0800

    tri

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,9 +1,26 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
-* main - does some thing
-* Return: 0
-*/
-int main(void)
+ * print_triangle - print right angle triangle
+ * @size: the side size
+ * Return: 0
+ */
+void print_triangle(int size)
 {
-	return (0);
+	int x, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++)
+		{
+			_putchar(y > size - x - 2 ? '#' : ' ');
+		}
+		_putchar('\n');
+	}
 }
