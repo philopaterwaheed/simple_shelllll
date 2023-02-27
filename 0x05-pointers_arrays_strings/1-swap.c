3ac16ad864333fa72d9d3513dcf1467927b69e8b
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-27 15:11:42 -0800

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,12 +2,12 @@
 /**
 * swap_int - does some thing
 * @a: ptr1
-* @b: ptr2        
+* @b: ptr2
 * Return: 0
 */
 void swap_int(int *a, int *b)
 {
 	int tmp = *a;
 	*a = *b;
-	*b = tmp;	
+	*b = tmp;
 }
