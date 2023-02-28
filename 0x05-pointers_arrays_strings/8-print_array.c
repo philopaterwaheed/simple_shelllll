53b4397e10d6d8bf982c53d328ecb9800e505402
philopater waheed <philopaterwaheed9@gmail.com>
2023-02-28 03:31:08 +0200

    m

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,11 +10,11 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (i = 0; i < n-1; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", *(a + i));
-		if (i < n - 1)
+		if (i < n-1)
 			printf(", ");
 	}
-
+	printf("\n");
 }
