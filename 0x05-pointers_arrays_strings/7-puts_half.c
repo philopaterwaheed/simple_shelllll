f95a76b524dcfd9dda1f0a3125d06cbffcde41cf
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-02-27 16:48:39 -0800

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,9 +1,24 @@
-#include <stdio.h>
+#include "main.h"
 /**
-* main - does some thing
-* Return: 0
-*/
-int main(void)
+ * rev_string - reverse string
+ * @s: the reference to the string
+ */
+void rev_string(char *s)
 {
-	return (0);
+	int len = 0, i, mid;
+	char c = *s;
+
+	while (c != '\0')
+		c = *(s + ++len);
+
+	if (len == 0)
+		return;
+
+	mid = len / 2;
+
+	for (i = mid; i < len; i++)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
 }
