2eda5dd3c2fa1d9618ef56923571db43c5a5f07b
Your Name <you@example.com>
2023-03-03 03:25:39 +0200

    s

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,13 @@
-#include <stdio.h>
 /**
-* main - does some thing
-* Return: 0
+* _strcmp - does some thing
+* @dest: detneation
+* @src:	sourc
+* Return: @dest
 */
-int main(void)
+int _strcmp(char *s1, char *s2)
 {
-	return (0);
+	for (; *s1 && *s2 && *s1 == *s2; s1++, s2++)
+	{}
+
+	return (*s1 - *s2);
 }
