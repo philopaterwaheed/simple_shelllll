8bce30c8982435b241d7d0f4c3fb5418676a4544
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-02 16:09:36 -0800

    cpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n && *src; src++, dest++, i++)
 		*dest = *src;
-	for (i ; i < n; i++, dest++)
+	for (; i < n; i++, dest++)
 	*dest = 0;
 	return (r);
 }
