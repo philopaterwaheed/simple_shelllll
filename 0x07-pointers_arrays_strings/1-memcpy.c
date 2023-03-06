7923122ccac9d050b07054a6b45f0a8d883fd483
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-06 13:47:53 -0800

    cpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,10 +7,10 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *r = dest;
-	int i;
+	unsigned int i;
 
-	for (i = 0; i < n && *src; src++, dest++, i++)
-		*dest = *src;
-	return (r);
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+
+	return (dest);
 }
