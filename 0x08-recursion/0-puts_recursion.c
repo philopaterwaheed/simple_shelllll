7b3c2bf285249f4d96bc701601a6a88e6ffd1180
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-08 12:51:47 -0800

    fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
 * void _puts_recursion - does some thing
+* @s: str
 * Return: 0
 */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 		return;
+
 	_puts_recursion(s ++);
 }
