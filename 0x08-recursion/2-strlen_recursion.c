305da1e8429c499cb6442cae2de942e13b61f969
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-08 13:31:03 -0800

    lenstr

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 /**
-* main - does some thing
+* _strlen_recursion - does some thing
+* @s: str
 * Return: 0
 */
-int main(void)
+int _strlen_recursion(char *s)
 {
-	return (0);
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (_print_rev_recursion(s + 1) + 1);
 }
