82f244f861ffe1711b963e4d449f74e5b9f259d5
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-08 14:27:35 -0800

    beat

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,11 +6,12 @@
 * Return:int
 */
 int check(int a, int b)
-{	if (a * a == b)
+{
+	if (a * a == b)
 		return (a);
 	if (a * a > b)
 		return (-1);
-	return (check(a + 1, b));	
+	return (check(a + 1, b));
 }
 /**
 * _sqrt_recursion - does some thing
