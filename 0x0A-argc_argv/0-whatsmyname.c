2d93ad1a480d3102bdbabf42a45d4cd85b2acff8
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-10 12:32:05 -0800

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
 * main - does some thing
+* @argc: argv
+* @argv: argc
 * Return: 0
 */
 int main(int argc, char **argv)
 {
 	(void) argc;
-	printf ("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
