14987c03ec9f0771040002cd5147fbc04bf71f7c
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-10 13:34:25 -0800

    sum

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
 * main - does some thing
+* @argc: argv
+* @argv: argc
 * Return: 0
 */
-int main(void)
+int main(int argc, char **argv)
 {
+	int i, n, sum = 0;
+	char *end;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; argv[i]; i++)
+	{
+		n = strtol(argv[i], &end, 10);
+		if (*end)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+
 	return (0);
 }
