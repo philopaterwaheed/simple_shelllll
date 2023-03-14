ad04844e5830b81d23e1670e432ce4f419e051ba
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-14 14:24:24 -0700

    dup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,20 +2,27 @@
 #include <string.h>
 #include <stdlib.h>
 /**
-* create_array - does some thing
-* @size: size
-* @c: char
+* _strdup - does some thing
+* @str: size
 * Return: 0
 */
-char *create_array(unsigned int size, char c)
+char *_strdup(char *str)
 {
 	char *o;
+	int i;
 
 	if (!size)
 		return (NULL);
 
 	o = malloc(size * sizeof(char));
 	if (c)
-		memset(o, c, size);
+	{
+		for (i = 0;;i++)
+		{
+			*(o + i); = *(str + i);
+			if (! *str)
+				break;
+		}
+	}
 	return (o);
 }
