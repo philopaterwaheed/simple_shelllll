3ca073f31297dc8cbc49d9c4c795609e0a88da9a
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-14 15:13:08 -0700

    fixes

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,10 +13,9 @@ char *str_concat(char *s1, char *s2)
 	int len1;
 	int len2;
 
-	if (!s1 || !s2)
-		return (NULL);
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+
+	len1 = !s1 ? 0 : strlen(s1);
+	len2 = !s2 ? 0 : strlen(s2);	
 	o = malloc(len1 + len2 + 1);
 	if (!o)
 		return (NULL);
