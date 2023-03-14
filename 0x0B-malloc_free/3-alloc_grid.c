d78cc8c0e8a22d5f5014ebf208c29be9973ef97f
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-14 16:58:10 -0700

    2d

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,9 +1,32 @@
-#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
 /**
-* main - does some thing
+* alloc_grid - does some thing
+* @width: size
+* @height: size
 * Return: 0
 */
-int main(void)
+int **alloc_grid(int width, int height)
 {
-	return (0);
+	int **o;
+	int len;
+	int i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	len = sizeof(int *) * height;	
+	o = malloc(len);
+	if (!o)
+		return (NULL);
+	if (o)
+	{
+		for (i = 0; i < height ; i++)
+		{
+			o[i] = calloc(0, sizeof(int *) * width);
+		}
+	}
+
+	return (o);
 }
