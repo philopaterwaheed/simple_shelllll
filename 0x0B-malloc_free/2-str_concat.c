1e6187a5969c1cf19eb93f4bf775031792334a63
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-14 15:15:43 -0700

    o

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,8 +19,11 @@ char *str_concat(char *s1, char *s2)
 	o = malloc(len1 + len2 + 1);
 	if (!o)
 		return (NULL);
-	strcpy(o, s1);
-	strcpy(o + len1, s1);
+
+	if (s1)
+		strcpy(o, s1);
+	if (s2)
+		strcpy(o + len1, s2);	
 	o[len1 + len2] = '\0';
 	return (o);
 }
