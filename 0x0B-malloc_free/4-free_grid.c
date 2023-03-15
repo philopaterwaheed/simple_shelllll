73386bf34d0d2cd83faee4fe4806eb2ce3d17cf2
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-14 17:20:37 -0700

    free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,9 +1,13 @@
-#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
 /**
-* main - does some thing
+* free_grid - does some thing
+* @grid: size
+* @height: char
 * Return: 0
 */
-int main(void)
+void free_grid(int **grid, int height);
 {
-	return (0);
+	free(grid);
 }
