ec6b351c40edfcc70f7d527e27b06e22a7c133e2
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-16 17:14:54 -0700

    init

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* malloc_checked - does some thing
+* @b: b
+* Return: 0
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (!ptr)
+		exit(98);
+	return (ptr);
+}
