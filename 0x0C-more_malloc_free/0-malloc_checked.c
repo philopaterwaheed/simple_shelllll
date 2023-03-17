6dd5376ac3e819dbbe41d5d44ac3d1ccac14770c
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-16 17:25:21 -0700

    fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,15 @@
+
 #include <stdlib.h>
+
 /**
-* malloc_checked - does some thing
-* @b: b
-* Description:
-* allocates memory of size @b and returns address points to the memory
-* otherwise exits with status code 98
-* Return: 0
-*/
+ * malloc_checked - allocate memory of size @b else exit
+ * @b: the size of the memory to allocate
+ * Description:
+ * allocates memory of size @b and returns address points to the memory
+ * otherwise exits with status code 98
+ *
+ * Return: the address of the allocated memory of success
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
