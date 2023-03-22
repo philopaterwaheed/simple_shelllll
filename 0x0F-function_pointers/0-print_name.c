945a758817fc1ce7f5f5330f262e721b1d987302
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-22 16:41:17 -0700

    fixed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,11 @@
 /**
-* print_name - does some thing
-* @name: ad
-* @f: pointer
-*/
+ * print_name - prints a name using util function @f
+ * @name: the name characters to print
+ * @f: the util function to print the name
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (f)
 		(*f)(name);
 }
+
