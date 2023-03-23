d2bb8701b07f58014e30f414eb4042e01900b75b
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-03-22 17:16:15 -0700

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,9 +1,43 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include "3-calc.h"
+
 /**
-* main - does some thing
-* Return: 0
-*/
-int main(void)
+ * main - program that preforms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: exit status
+ */
+int main(int argc, char **argv)
 {
+	int a, b;
+	char *op;
+	int (*op_fun)(int, int);
+
+	if (argc != 4)
+	{
+		puts("Error");
+		return (98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op = argv[2];
+	op_fun = get_op_func(op);
+
+	if (!op_fun)
+	{
+		puts("Error");
+		return (99);
+	}
+
+	if ((*op == '/' || *op == '%') && !b)
+	{
+		puts("Error");
+		return (100);
+	}
+
+	printf("%d\n", op_fun(a, b));
 	return (0);
 }
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+/**
+ * op_add - add operator function
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the sum of @a and @b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub operator function
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the diff of @a and @b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul operator function
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the multiplication of @a and @b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - div operator function
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the division of @a and @b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod operator function
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the division remaining of @a and @b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
