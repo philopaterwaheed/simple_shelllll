e40bb2e6e3531792ce2927aeecf447676a9556ad
Your Name <you@example.com>
2023-03-24 18:15:51 +0200

    init

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+* sum_them_all - does some thing
+* @n: char
+* Return: 0
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i; 
+	int sum;
+	va_list list;
+
+	if (n <= 0)
+		return (0);
+	
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
+
+	va_end(list);
+	return (sum);
+}
