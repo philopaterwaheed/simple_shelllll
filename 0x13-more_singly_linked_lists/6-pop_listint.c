8dd30acb9136e5c3d945128c275aeedc258d3406
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-04-04 16:55:04 -0700

    pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,9 +1,30 @@
+#include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
-* main - does some thing
+* pop_listint - does some thing
+* @head: l
 * Return: 0
 */
-int main(void)
+int pop_listint(listint_t **head)
 {
-	return (0);
+	int n;
+	listint_t *t;
+
+	if (head == NULL)
+		return (0);
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->next = NULL;
+	if (*head == 0)
+	{
+		return (0);
+	}
+	t = head;
+	head = t->next;
+	n = t->n;
+	free(t);
+	return (n);
 }
