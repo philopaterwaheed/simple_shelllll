dd686cf842cbd5b56519b34aa3c25e5561a109ab
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-04-04 14:51:10 -0700

    init

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_listint - does some thing
+* @h: l
+* Return: 0
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while(h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
