c3e0d2e22fe817bcdb727e9bd672a74c21cb5601
Your Name <you@example.com>
2023-04-05 21:07:30 +0200

    dsa

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _realloc - reallocates
+ *
+ * @ptr: pointer
+ * @old_size:is the size
+ * @new_size: size
+ * Return: pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+	unsigned int i, n = new_size;
+	char *optr = ptr;
+
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		return (p);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (new_size == old_size)
+		return (ptr);
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	if (new_size > old_size)
+		n = old_size;
+	for (i = 0; i < n; i++)
+		p[i] = optr[i];
+	free(ptr);
+	return (p);
+}
