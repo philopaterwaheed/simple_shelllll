2c0e302b21e00e34abdba94a577a4de434ea7142
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-04-05 13:34:07 -0700

    len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - prints
+ * @h: the first
+ * Return: the size 
+ */
+size_t list_len(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		h = h->next;
+		size++;
+	}
+
+	return (size);
+}
