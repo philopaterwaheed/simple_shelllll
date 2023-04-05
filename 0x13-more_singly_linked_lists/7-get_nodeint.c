1c72ed8b0e46591d696c9ebd315d9df3e6461829
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-04-04 17:05:13 -0700

    del_n

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,9 +1,20 @@
+#include "lists.h"
 #include <stdio.h>
 /**
-* main - does some thing
+* get_nodeint_at_index - does some thing
+* @h: l
 * Return: 0
 */
-int main(void)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	return (0);
+	size_t count = 0;
+
+	while (count < n)
+	{
+		h = h->next;
+		count++;
+		if (h == NULL)
+			return (NULL);
+	}
+	return (h);
 }
