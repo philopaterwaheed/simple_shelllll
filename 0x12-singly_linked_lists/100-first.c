f9804584062b1bbfe967963e7ff5504652e9568e
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-04-05 13:57:05 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * premain - doessomething 
+ */
+void __attribute__((constructor)) premain(void)
+{
+	puts("You're beat! and yet, you must allow,");
+	puts("I bore my house upon my back!");
+}
