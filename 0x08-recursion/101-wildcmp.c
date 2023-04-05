709240c8d2ac9d376b0af7b7e612725443da5438
Your Name <you@example.com>
2023-04-05 20:36:13 +0200

    cmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -17,7 +17,7 @@ int st_ch(char *s1, char *s2, int i, int j)
 	if (s1[i] == '\0' && s2[j] == '*')
 		return (st_ch(s1, s2, i, j + 1));
 	if (s2[j] == '*')
-		return (st_ch(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+		return (st_ch(s1, s2, i + 1, j) || st_ch(s1, s2, i, j + 1));
 	return (0);
 }
 /**
