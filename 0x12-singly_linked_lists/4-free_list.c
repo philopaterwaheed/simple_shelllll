c3a3f33ff7db7623070980fbbd50cbb556c0972b
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-04-05 13:51:15 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - free
+ * @head: A pointer
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
+}
