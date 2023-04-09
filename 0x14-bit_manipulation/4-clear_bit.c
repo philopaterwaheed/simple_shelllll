57d33015e5aa724de6f7885bd3db57ca6ab6255a
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-04-09 09:51:32 -0700

    fixes

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,6 +11,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
-	((*n &= ~(1UL << index)) != 0);
+	*n &= ~(1UL << index);
 	return (1);
 }
