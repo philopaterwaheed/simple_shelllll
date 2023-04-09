545709f01fe8980875c5d8a92d85140f05f5d606
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-04-09 09:29:22 -0700

    pop

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,8 +9,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8) {
-		return false; // index is out of range
-    }
+	if (index >= sizeof(unsigned long int) * 8)
+		return (0);
 	return  ((n & (1 << index)) != 0);
 }
