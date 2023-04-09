5db2020d1613c0e200cdce9ae358d401baa9d57a
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-04-09 08:34:18 -0700

    init

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,29 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+
 /**
  * binary_to_uint - convert binary int
  * @b: binary
+ *
  * Return: decimal
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, n = 0, box;
+        unsigned int i, n = 0, box;
 
-	if (!b)
-		return (n);
+        if (!b)
+                return (n);
 
-	for (i = strlen(b) - 1, box = 1; (int)i >= 0; i--, box *= 2)
-		if (b[i] == '1')
-			n += box;
-		else if (b[i] == '0')
-			continue;
-		else
-			return (0);
-	return (n);
+        for (i = strlen(b) - 1, box = 1; (int)i >= 0; i--, box *= 2)
+        {
+                if (b[i] == '1')
+                        n += box;
+                else if (b[i] == '0')
+                        continue;
+                else
+                        return (0);
+        }
+
+        return (n);
 }
