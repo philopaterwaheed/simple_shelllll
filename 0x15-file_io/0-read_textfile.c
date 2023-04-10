71b767a15ae20afa7a9bafbcdbc28fb1425dce0f
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-04-10 16:51:20 -0700

    commit_message

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stddef.h>
+
 #include <stdlib.h>
 /**
  * read_textfile - Reads a text file
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,10 @@
-ssize_t read_textfile(const char *filename, size_t letters);
-
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
+ #include <sys/types.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+ssize_t read_textfile(const char *filename, size_t letters);
