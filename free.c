8d0928b63a9b1e7dfa820d953054603380ac30c1
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-08-24 04:06:38 +0000

    frees

---
+++ free.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * free_arguments - frees arguments
+ * @arguments: the arguments inputed
+ * @first: the first char to the commands
+ * Return: the first argument
+ */
+void free_arguments(char **arguments, char **first)
+{
+	int    i;
+	printf ("hehe");
+	for (i = 0; /*arguments[i] || arguments[i + 1]*/i<= array_size(arguments); i++)
+		free(arguments[i]);
+
+	free(first);
+}
+/**
+ * free_arguments - frees env
+ * Return: the first env
+ */
+void free_env(void)
+{
+	/* 
+	 * i being the index
+	 */
+	int i;
+
+	for (i= 0; /*environ[i]*/i<= array_size(environ); i++)
+		free(environ[i]);
+	free(environ);
+}
