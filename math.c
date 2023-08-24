b3520e9481de470dadde4d848c45cf767c5353bc
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-08-24 03:43:26 +0000

    math helper function

--- math.c
+++ math.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * min - math helper function to be used in realloc
+ * @a: the first number
+ * @b: the seconed number
+ * Return: the minmum number.
+ */
 int min(int a, int b)
 {
 	if(a < b)
